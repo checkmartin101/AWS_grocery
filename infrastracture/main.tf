771a848c3121414345885d18e71d439e2d9af75d
Adewale martin Adedeji <martinadewaleadedeji@hotmail.com>
2026-04-27 11:26:38 +0200

    Update main.tf

--- infrastracture/main.tf
+++ infrastracture/main.tf
@@ -132,7 +132,7 @@ resource "aws_iam_instance_profile" "adewale_iam_instance_profile" {
 }
 resource "aws_instance" "adewale-ec2" {
     ami = "ami-0f50f13aefb6c0a5d"
-    instance_type = "t3.micro"
+    instance_type = var.instance_type
     subnet_id = aws_subnet.public_subnet.id
     associate_public_ip_address = true
     iam_instance_profile = aws_iam_instance_profile.adewale_iam_instance_profile.name
